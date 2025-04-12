9d4183dc13a557272db3b56330c3b2a5f618e178
Sean Winston <seanwinston@gmail.com>
2025-04-12 14:39:55 +0100

    feat: change path to global.properties

--- src/test/java/utils/Utils.java
+++ src/test/java/utils/Utils.java
@@ -34,7 +34,7 @@ public class Utils {
 
     public static String getGlobalValue(String key) throws IOException {
         Properties prop = new Properties();
-        FileInputStream fis = new FileInputStream("/Users/seanwinston/AcademyRepos/SpartaAPITestFramework/src/test/java/utils/global.properties");
+        FileInputStream fis = new FileInputStream("src/test/java/utils/global.properties");
         prop.load(fis);
         return prop.getProperty(key);
     }
