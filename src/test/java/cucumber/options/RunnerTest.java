4c0df04f38f9370be789353739a2a2c401952f28
Sean Winston <seanwinston@gmail.com>
2025-04-12 12:12:00 +0100

    refactor: RunnerTest is more compatible w junit5 and now sends reports to target/cucumber-html-reports

--- src/test/java/cucumber/options/RunnerTest.java
+++ src/test/java/cucumber/options/RunnerTest.java
@@ -1,10 +1,17 @@
 package cucumber.options;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features",glue = {"stepdefinitions"})
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "stepdefinitions")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, json:target/jsonReports/cucumber-report.json")
 public class RunnerTest {
 }
