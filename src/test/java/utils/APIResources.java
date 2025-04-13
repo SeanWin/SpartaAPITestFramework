43d0d45f6667c63074ee70218cb6e1b3942a4ec8
Sean Winston <seanwinston@gmail.com>
2025-04-13 20:51:57 +0100

    feat: add spartanById endpoint to APIResources

--- src/test/java/utils/APIResources.java
+++ src/test/java/utils/APIResources.java
@@ -4,7 +4,8 @@ public enum APIResources {
     getCourses("/api/courses"),
     getCourse("/api/courses/{id}"),
     authentication("/Auth/login"),
-    spartan("/api/spartans");
+    spartan("/api/spartans"),
+    spartanById("/api/spartans/{id}");
     private String resource;
 
     APIResources(String resource) {
