34e39344e917675aef41c3393d2eac9d38be0557
Sean Winston <seanwinston@gmail.com>
2025-04-14 10:32:44 +0100

    feat: add afterScenario hook which deletes the spartan after a POST request

--- src/test/java/stepdefinitions/Hooks.java
+++ src/test/java/stepdefinitions/Hooks.java
@@ -1,5 +1,6 @@
 package stepdefinitions;
 
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
 import java.io.IOException;
@@ -14,5 +15,12 @@ public class Hooks {
             m.user_calls_endpoint_with_http_request("authentication","POST");
             m.the_response_contains_an_authentication_token();
         }
+    }
+
+    @After("@DeleteSpartanAfter")
+    public void afterScenario() throws IOException {
+        StepDefs m = new StepDefs();
+        m.spartan_endpoint_is_up_and_user_is_authenticated();
+        m.user_calls_endpoint_with_http_request_for_course_id("spartanById","DELETE",StepDefs.createdSpartanId);
     }
 }
