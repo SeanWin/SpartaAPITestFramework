ad13c39e5a2f53727540a17ffeb9a582cbe9cbbd
Sean Winston <seanwinston@gmail.com>
2025-04-16 14:38:28 +0100

    feat: change TestDataBuild class so it can handle other valid stream names

--- src/test/java/utils/TestDataBuild.java
+++ src/test/java/utils/TestDataBuild.java
@@ -8,24 +8,13 @@ public class TestDataBuild {
 
     public SpartanRequest createSpartanPayload(String firstName, String lastName, String streamName) {
 
-        Stream stream = new Stream();
-        stream.setName(streamName);
-
-        CourseRequest course = new CourseRequest();
-        course.setName("TECH 302");
-        course.setTrainer("Nish Mandal");
-        course.setStartDate("2023-05-01T00:00:00");
-        course.setEndDate("2023-07-01T00:00:00");
-        course.setStream(stream);
-        course.setId(3);
-
         SpartanRequest spartan = new SpartanRequest();
         spartan.setFirstName(firstName);
         spartan.setLastName(lastName);
         spartan.setUniversity("Uni");
         spartan.setGraduated(true);
         spartan.setDegree("degree");
-        spartan.setCourse(course);
+        spartan.setCourse(buildCourse(streamName));
         spartan.setId(34);
 
         return spartan;
@@ -33,26 +22,60 @@ public class TestDataBuild {
 
     public SpartanRequest createSpartanPayload(int id, String firstName, String lastName, String streamName) {
 
-        Stream stream = new Stream();
-        stream.setName(streamName);
-
-        CourseRequest course = new CourseRequest();
-        course.setName("TECH 302");
-        course.setTrainer("Nish Mandal");
-        course.setStartDate("2023-05-01T00:00:00");
-        course.setEndDate("2023-07-01T00:00:00");
-        course.setStream(stream);
-        course.setId(3);
-
         SpartanRequest spartan = new SpartanRequest();
         spartan.setFirstName(firstName);
         spartan.setLastName(lastName);
         spartan.setUniversity("Uni");
         spartan.setGraduated(true);
         spartan.setDegree("degree");
-        spartan.setCourse(course);
+        spartan.setCourse(buildCourse(streamName));
         spartan.setId(id);
 
         return spartan;
+    }
+
+    private CourseRequest buildCourse(String streamName) {
+        Stream stream = new Stream();
+        stream.setName(streamName);
+
+        CourseRequest course = new CourseRequest();
+        course.setStream(stream);
+
+        switch (streamName.toLowerCase()) {
+
+            case "c# dev":
+                course.setName("TECH 300");
+                course.setTrainer("Phil Windridge");
+                course.setStartDate("2023-03-01T00:00:00");
+                course.setEndDate("2023-05-01T00:00:00");
+                course.setId(1);
+                break;
+
+            case "java dev":
+                course.setName("TECH 301");
+                course.setTrainer("Catherine French");
+                course.setStartDate("2023-04-01T00:00:00");
+                course.setEndDate("2023-06-01T00:00:00");
+                course.setId(2);
+                break;
+
+            case "devops":
+                course.setName("TECH 303");
+                course.setTrainer("Abdul Shahrukh Khan");
+                course.setStartDate("2023-06-01T00:00:00");
+                course.setEndDate("2023-08-01T00:00:00");
+                course.setId(4);
+                break;
+
+            default:
+                course.setName("TECH 302");
+                course.setTrainer("Nish Mandal");
+                course.setStartDate("2023-05-01T00:00:00");
+                course.setEndDate("2023-07-01T00:00:00");
+                course.setId(3);
+                break;
+        }
+
+        return course;
     }
 }
